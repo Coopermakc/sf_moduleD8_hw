fed181112b4833ca5bc1a73c32417d9161f76480
Coopermakc <Coopermakc@mail.ru>
2020-11-01 21:01:49 +0300

    cahced view

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from django.views.decorators.cache import cache_page
+
 
 
 from . import views
@@ -11,5 +11,5 @@ urlpatterns = [
     path("list/", views.TaskListView.as_view(), name="list"),
     path("list/c/<slug:cat_slug>", views.tasks_by_cat, name="list_by_cat"),
     path("details/<int:pk>", views.TaskDetailsView.as_view(), name="details"),
-    path("cached/", cache_page(60*5)(views.cached), name="cached")
+    path("cached/", views.cached, name="cached")
 ]
